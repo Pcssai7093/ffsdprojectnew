98662a4b7a13e6d818fb91fe753346dd5efda2a8
Potti Chandra Sekhar Sai <91785676+Pcssai7093@users.noreply.github.com>
2022-03-23 22:21:22 +0530

    23/3/22 10:19

--- postgig.js
+++ postgig.js
@@ -1,1 +1,21 @@
 // * to be done
+
+
+let realbtn=document.getElementsByClassName('image-input');
+let custbtn=document.getElementsByClassName('Cbtn');
+let custtxt=document.getElementsByClassName('Ctxt');
+
+
+for(let i=0;i<custbtn.length;i++)
+custbtn[i].addEventListener('click',function(){
+    realbtn[i].click();
+
+    realbtn[i].addEventListener('change',function(){
+        if(realbtn[i].value!=''){
+            custtxt[i].innerText=realbtn[i].value;
+        }
+    });
+
+
+}); 
+
